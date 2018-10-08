d23907e7ec9305b500930da6e89baceac7717c84
Alex Corey <alex.swansboro@gmail.com>
2018-10-08 14:17:06 -0400

    committing for merge

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Label, Input, Notification, Button } from 'bloomer'
 import { NavLink } from 'react-router-dom'
-import api from './api
+import api from './api'
+// import Register from './register'
 // import styles from './App.css'
 
 class Login extends Component {
@@ -28,44 +29,39 @@ class Login extends Component {
   }
   render () {
     const { email, password, errMsg } = this.state
-    if (this.state.registering) {
-      return (<Register setRegister={this.setRegister()} />
-      )
-    } else {
-      return (
-        <div className='loginContainer'>
-          <div className='header' />
-          <div className='toggleLogin'>
-            <span to='/login'><strong className='toggleLogin'>login</strong></span>
-            <span className='pipe'> |</span>&nbsp;
-            <NavLink className='toggleLogin' to='/register' onClick={e => this.setRegistering(e, true)}>register</NavLink>
-          </div>
-          { errMsg &&
+    return (
+      <div className='loginContainer'>
+        <div className='header' />
+        <div className='toggleLogin'>
+          <span to='/login'><strong className='toggleLogin'>login</strong></span>
+          <span className='pipe'> |</span>&nbsp;
+          <NavLink className='toggleLogin' to='/register' onClick={e => this.setRegistering(e, true)}>register</NavLink>
+        </div>
+        { errMsg &&
             <Notification isColor='warning'>
               <div>{errMsg}</div>
             </Notification>
-          }
-          <div className='loginField'>
-            <label className='emailLabel'>email</label><br />
-            <input className='emailInput' value={email} placeholder='manager@example.com' type='email' onChange={e => this.setState({ email: e.target.value })} required /><br />
-            <label className='passwordLabel'>password</label><span className='forgotPassword'><a href='#'>Forgot password?</a></span><br />
-            <input className='passwordInput' value={password} placeholder='Must use at least 5 characters' type='password' onChange={e => this.setState({ password: e.target.value })} required /><br />
-            <div className='introMessage'>
-              <h1 className='description'>
+        }
+        <div className='loginField'>
+          <label className='emailLabel'>email</label><br />
+          <input className='emailInput' value={email} placeholder='manager@example.com' type='email' onChange={e => this.setState({ email: e.target.value })} required /><br />
+          <label className='passwordLabel'>password</label><span className='forgotPassword'><a href='#'>Forgot password?</a></span><br />
+          <input className='passwordInput' value={password} placeholder='Must use at least 5 characters' type='password' onChange={e => this.setState({ password: e.target.value })} required /><br />
+          <div className='introMessage'>
+            <h1 className='description'>
                 Shift work coordination meets online <br />
                 solutions customized for small business. <br />
                 Login or sign up to
-                <NavLink className='register' to='/register'
-                  onClick={e => this.setRegistering(e, true)}
-                > register</NavLink>!</h1>
-            </div>
+              <NavLink className='register' to='/register'
+                onClick={e => this.setRegistering(e, true)}
+              > register</NavLink>!</h1>
           </div>
-          <NavLink to='/CalendarList'>
-            <button className='loginButton' onClick={e => { this.handleSubmit(e) }}>Login</button>
-          </NavLink>
         </div>
-      )
-    }
+        <NavLink to='/CalendarList'>
+          <button className='loginButton' onClick={e => { this.handleSubmit(e) }}>Login</button>
+        </NavLink>
+      </div>
+    )
   }
 }
 
