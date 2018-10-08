8d5996a800894a364ac2bde2ded995ae7020ffc4
Alex Corey <alex.swansboro@gmail.com>
2018-10-08 13:50:37 -0400

    Accepting shifts is rending buttons that are disabled as necessary.

--- src/components/AcceptShiftRequest.js
+++ src/components/AcceptShiftRequest.js
@@ -29,17 +29,30 @@ class AcceptShiftRequest extends Component {
   }
   render () {
     let { shiftSwapsIndex } = this.state
+
     return (
       <div>
   Click on a Shift to Accept It
-        {shiftSwapsIndex.map((shiftSwap) =>
-
-          <div key={shiftSwap.id}>
-            <Button value={shiftSwap.id}
-              onClick={(e) => { if (window.confirm('Accept this shift?')) this.acceptShiftSwap(e, e.target.value) }}
-            >{moment(shiftSwap.shift_start_time).format('MMM Do YYYY hh:mm a')} - {moment(shiftSwap.shift_end_time).format('MMM Do YYYY hh:mm a')}</Button>
-          </div>
-        )}
+        {shiftSwapsIndex.map((shiftSwap) => {
+          if (shiftSwap.accepting_user) {
+            return (<div>
+              <div key={shiftSwap.id}>
+                <Button disabled={shiftSwap.accepting_user} value={shiftSwap.id}
+                  onClick={(e) => { if (window.confirm('Accept this shift?')) this.acceptShiftSwap(e, e.target.value) }}
+                >{moment(shiftSwap.shift_start_time).format('MMM Do YYYY hh:mm a')} - {moment(shiftSwap.shift_end_time).format('MMM Do YYYY hh:mm a')}</Button>
+              </div>
+            </div>)
+          } else {
+            return (
+              <div>
+                <div key={shiftSwap.id}>
+                  <Button isOutlined='success' value={shiftSwap.id}
+                    onClick={(e) => { if (window.confirm('Accept this shift?')) this.acceptShiftSwap(e, e.target.value) }}
+                  >{moment(shiftSwap.shift_start_time).format('MMM Do YYYY hh:mm a')} - {moment(shiftSwap.shift_end_time).format('MMM Do YYYY hh:mm a')}</Button>
+                </div>
+              </div>)
+          }
+        })}
       </div>
     )
   }
