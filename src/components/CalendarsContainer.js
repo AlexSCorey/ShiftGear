cf80e29ad57e9c579b582bb2f28e8118d0262acf
Alex Corey <alex.swansboro@gmail.com>
2018-10-12 13:53:08 -0400

    commit to merge

--- src/components/CalendarsContainer.js
+++ src/components/CalendarsContainer.js
@@ -24,7 +24,7 @@ class CalendarsContainer extends Component {
 
   render () {
     const { calendars } = this.state
-    const { currentUser } = this.props
+    // const { currentUser } = this.props
     let calendarTypes = Object.keys(calendars)
     if (calendarTypes.length > 0) {
       const calendarNames = {
@@ -32,33 +32,95 @@ class CalendarsContainer extends Component {
         owned_calendars: 'Owned Calendars',
         employed_calendars: 'Employed Calendars'
       }
-      return (
-        <div key={'calendarType'}>
-          <div className='listItems'>
-            <Link className='title' to='/CreateCalendar'><button className='titleButton'>New Calendar</button></Link>
-          </div>
-          { calendarTypes.map((calendarType) => {
-            let calendarGroup = calendars[calendarType]
-            if (calendarGroup.length > 0) {
-              return (
-                <div>
-                  <div key={calendarGroup.id} >
-                    <h1 className='titles'>{calendarNames[calendarType]}</h1>
-                    <CalendarList key={calendarGroup.id} type={calendarNames[calendarType]} calendarGroup={calendarGroup} />
+      if (calendars.managed_calendars.length > 0 || calendars.owned_calendars.length > 0) {
+        return (
+          <div key={'calendarType'}>
+            <div className='listItems'>
+              <Link className='title' to='/CreateCalendar'><button className='titleButton'>New Calendar</button></Link>
+            </div>
+            { calendarTypes.map((calendarType) => {
+              let calendarGroup = calendars[calendarType]
+              if (calendarGroup.length > 0) {
+                return (
+                  <div>
+                    <div key={calendarGroup.id} >
+                      <h1 className='titles'>{calendarNames[calendarType]}</h1>
+                      <CalendarList key={calendarGroup.id} type={calendarNames[calendarType]} calendarGroup={calendarGroup} />
+                    </div>
+                    <div className='center'><Link to={`Calendar/UpdateProfile`}><button className='navButtons'>Update Your Profile</button></Link></div>
                   </div>
-                  <div className='center'><Link to={`Calendar/UpdateProfile`}><button className='navButtons'>Update Your Profile</button></Link></div>
-                </div>
-              )
-            } else {
-              return ('')
-            }
-          }) }
-        </div>
-      )
+                )
+              } else {
+                return ('')
+              }
+            }) }
+          </div>
+        )
+      } else {
+        return (
+          <div key={'calendarType'}>
+            <div className='listItems' />
+            { calendarTypes.map((calendarType) => {
+              let calendarGroup = calendars[calendarType]
+              if (calendarGroup.length > 0) {
+                return (
+                  <div>
+                    <div key={calendarGroup.id} >
+                      <h1 className='titles'>{calendarNames[calendarType]}</h1>
+                      <CalendarList key={calendarGroup.id} type={calendarNames[calendarType]} calendarGroup={calendarGroup} />
+                    </div>
+                    <div className='center'><Link to={`Calendar/UpdateProfile`}><button className='navButtons'>Update Your Profile</button></Link></div>
+                  </div>
+                )
+              } else {
+                return ('')
+              }
+            }) }
+          </div>
+        )
+      }
     } else {
-      return (<div className='listItems'>
-        <Link className='title' to='/CreateCalendar'><button className='titleButton'>New Calendar</button></Link></div>)
+      return (<div>Loading</div>)
     }
+
+  // render () {
+  //   const { calendars } = this.state
+  //   // const { currentUser } = this.props
+  //   let calendarTypes = Object.keys(calendars)
+  //   if (calendarTypes.length > 0) {
+  //     const calendarNames = {
+  //       managed_calendars: 'Managed Calendars',
+  //       owned_calendars: 'Owned Calendars',
+  //       employed_calendars: 'Employed Calendars'
+  //     }
+  //     return (
+  //       <div key={'calendarType'}>
+  //         <div className='listItems'>
+  //           <Link className='title' to='/CreateCalendar'><button className='titleButton'>New Calendar</button></Link>
+  //         </div>
+  //         { calendarTypes.map((calendarType) => {
+  //           let calendarGroup = calendars[calendarType]
+  //           if (calendarGroup.length > 0) {
+  //             return (
+  //               <div>
+  //                 <div key={calendarGroup.id} >
+  //                   <h1 className='titles'>{calendarNames[calendarType]}</h1>
+  //                   <CalendarList key={calendarGroup.id} type={calendarNames[calendarType]} calendarGroup={calendarGroup} />
+  //                 </div>
+  //                 <div className='center'><Link to={`Calendar/UpdateProfile`}><button className='navButtons'>Update Your Profile</button></Link></div>
+  //               </div>
+  //             )
+  //           } else {
+  //             return ('')
+  //           }
+  //         }) }
+  //       </div>
+  //     )
+  //   } else {
+  //     return (<div className='listItems'>
+  //       <Link className='title' to='/CreateCalendar'><button className='titleButton'>New Calendar</button></Link></div>)
+  //   }
+  // }
   }
 }
 
